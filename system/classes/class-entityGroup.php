44ee8cda18897fc32df6ee45b6505f9ef254a080
tommy2018 <Tommy@Tommys-MacBook-Pro.local>
2015-07-30 16:35:54 +1000

    Update

--- system/classes/class-entityGroup.php
+++ system/classes/class-entityGroup.php
@@ -7,5 +7,32 @@ class EntityGroup {
 	
 	private function __construct()  {}
 	
+	static public function getEntityGroupListByEventID($eventID) {
+		$db = Database::getInstance();
+		$conn = $db->connect();
+		
+		$stmt = $conn->prepare('SELECT id, name FROM entity_group WHERE eventID = :eventID');
+		
+		if ($stmt->execute(array('eventID' => $eventID)))
+			if ($result = $stmt->fetchAll())
+				return $result;
+	}
+	
+	public function getID() {
+		return $this->id;
+	}
+	
+	public function getEventID() {
+		return $this->eventID;
+	}
+	
+	public function getName() {
+		return $this->name;
+	}
+	
+	public function getDescription() {
+		return $this->description;
+	}
+	
 }
 ?>
