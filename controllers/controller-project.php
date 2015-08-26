84197b683da2335e8add6f81828326c7f09f4d0e
tommy2018 <Tommy@cs321s-iMac-7.local>
2015-08-27 00:44:06 +1000

    New

--- controllers/controller-project.php
+++ controllers/controller-project.php
@@ -19,10 +19,10 @@ if (!isset($_GET['id'])) fatalError('Unable to find the resource you requested.'
 if (!preg_match('/^[1-9][0-9]*$/', $_GET['id'])) fatalError('Unable to find the resource you requested.', 404);
 
 $projectID = $_GET['id'];
-$entity = Entity::getEntityByID($projectID);
+$info = Entity::getEntityInfoByID($projectID);
 
-if (!$entity) fatalError('Unable to find the resource you requested.', 404);
-$template->assign('entity', $entity);
+if (!$info) fatalError('Unable to find the resource you requested.', 404);
+$template->assign('info', $info);
 
 $template->display('template-project');
 ?>
