fcb3f6d7260e94fa69695bb90372bb1e2051c574
tommy2018 <Tommy@cs321s-iMac-7.local>
2015-08-28 23:16:13 +1000

    Control Panel Update

--- scripts/control-panel/control-panel.js
+++ scripts/control-panel/control-panel.js
@@ -1,28 +1,78 @@
 // JavaScript Document
-var currentSettingGroup = '';
-var currentSettingPanel = '';
-
+var cpMenuToggle;
+var cpMenuToggleName;
+var cpMenuDropdown;
+var cpMenuDropdownArrow;
+var cpMenuDropdownItem;
+var cpOptionListItem;
 
 $(document).ready(function(e) {
-    
+	initControls();
+	
+	cpMenuDropdownItem.on('click', function() {
+		switchSideNavigation($(this).data('file'));
+		cpMenuToggleName.html($(this).data('name'));
+	});
+	
+	cpMenuToggle.on('click', function() {
+		if (cpMenuDropdown.is(":visible")) {
+			cpMenuDropdown.hide();
+			cpMenuDropdownArrow.removeClass('fa-chevron-up');
+			cpMenuDropdownArrow.addClass('fa-chevron-down');
+		} else {
+			cpMenuDropdown.show();
+			cpMenuDropdownArrow.removeClass('fa-chevron-down');
+			cpMenuDropdownArrow.addClass('fa-chevron-up');
+		}
+	});
 });
 
 function switchSettingPanel(fileName) {
-	return function() {
-		var filePath = 'html/' + fileName;
-		$.ajax({
-			url: filePath,
-			cache: false,
-			success: function(data) {
-				currentSettingPanel = fileName;
-				$('#control_panel_setting_area').html(data);
-			},
-			error: function() {
-				$('#control_panel_setting_area').html('ERROR');
-			}
-		});
-	}
+	var filePath = 'html/' + fileName;
+	
+	$.ajax({
+		url: filePath,
+		cache: false,
+		success: function(data) {
+			$('#control_panel_setting_area').html(data);
+		},
+		error: function() {
+			$('#control_panel_setting_area').html('ERROR');
+		}
+	});
 }
 
-function switchSettingGroup(fileName) {
+function switchSideNavigation(fileName) {
+	var filePath = 'html/' + fileName;
+	
+	$.ajax({
+		url: filePath,
+		cache: false,
+		success: function(data) {
+			$('#control_panel_options_list').html(data);
+			udpateOptionListControls();
+		},
+		error: function() {
+			$('#control_panel_options_list').html('ERROR');
+		}
+	});
+}
+
+function initControls() {
+	cpMenuToggle = $('#control_panel_menu_toggle');
+	cpMenuToggleName = $('#control_panel_menu_toggle_name');
+	cpMenuDropdown = $('#control_panel_menu_dropdown');
+	cpMenuDropdownArrow = $('#control_panel_menu_toggle_arrow');
+	cpMenuDropdownItem = $('#control_panel_menu_dropdown li');
+	cpOptionListItem = $('#control_panel_options_list div');
+}
+
+function udpateOptionListControls() {
+	cpOptionListItem = $('#control_panel_options_list div');
+	
+	cpOptionListItem.on('click', function() {
+		switchSettingPanel($(this).data('file'));
+		cpOptionListItem.removeClass('control_panel_options_list_selected');
+		$(this).addClass('control_panel_options_list_selected');
+	});
 }
--- templates/template-controlPanel.php
+++ templates/template-controlPanel.php
@@ -1,31 +1,16 @@
 <?php include_once 'template-header.php' ?>
-<script>
-$(document).ready(function(e) {
-	sideMenuOptions = [];
-	
-	sideMenuOptions.push(['#control_panel_option_project', 'control-panel/project-group/project-menu.html']);
-	sideMenuOptions.push(['#control_panel_option_user', 'control-panel/project-group/user-menu.html']);
-	sideMenuOptions.push(['#control_panel_option_role', 'control-panel/project-group/role-menu.html']);
-	
-	for (var i = 0; i < sideMenuOptions.length; i++)
-		$(sideMenuOptions[i][0]).on('click', function() {
-			switchSettingPanel(sideMenuOptions[i][1]);
-		
-		});
-});
-</script>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <div id="main">
   <div class="container-fluid" id="main_frame">
     <div class="row">
       <div class="col-md-12">
         <div class="card" id="control_panel_card">
           <div class="card_top">
-            <div class="card_title"> <span class="control_panel_menu"><i class="fa fa-cogs"></i> PROJECT GROUP SETTINGS &nbsp;<i class="fa fa-chevron-down"></i></span>
-              <div class="control_panel_menu_dropdown">
+            <div class="card_title"> <span id="control_panel_menu_toggle"><i class="fa fa-cogs"></i> <span id="control_panel_menu_toggle_name">APPLICATION SETTINGS</span> &nbsp;<i class="fa fa-chevron-down" id="control_panel_menu_toggle_arrow"></i></span>
+              <div id="control_panel_menu_dropdown">
                 <ul>
-                  <li>APPLICATION SETTINGS</li>
-                  <li>EVENT SETTINGS</li>
+                  <li data-name="APPLICATION SETTINGS" data-file="control-panel/side-menu/application.html">- APPLICATION SETTINGS</li>
+                  <li data-name="EVENT SETTINGS" data-file="control-panel/side-menu/event.html">- EVENT SETTINGS</li>
+                  <li data-name="PROJECT GROUP SETTINGS" data-file="control-panel/side-menu/project-group.html">- PROJECT GROUP SETTINGS</li>
                 </ul>
               </div>
             </div>
@@ -35,16 +20,11 @@ $(document).ready(function(e) {
               <div class="row">
                 <div class="col-lg-3">
                   <div id="control_panel_options_list_area">
-                    <div id="control_panel_options_list">
-                      <div id="control_panel_option_project" class="control_panel_options_list_selected control_panel_options_list_item"> <span><i class="fa fa-leaf control_panel_options_list_icon"></i> Project</span> </div>
-                      <div id="control_panel_option_user" class="control_panel_options_list_item"><i class="fa fa-user control_panel_options_list_icon"></i> User</div>
-                      <div id="control_panel_option_role" class="control_panel_options_list_item"><i class="fa fa-users control_panel_options_list_icon"></i> Role</div>
-                    </div>
+                    <div id="control_panel_options_list"> </div>
                   </div>
                 </div>
                 <div class="col-lg-9">
-                  <div id="control_panel_setting_area"> 
-                  </div>
+                  <div id="control_panel_setting_area"> </div>
                 </div>
               </div>
             </div>
